25695b33d72ad144439edefbf64fb763591dc183
Ashley Baldock <ashley@baldock.me>
2020-05-27 22:08:21 +0100

    Add in progress files

---
+++ src/Art/Art.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './Art.module.css';
+
+export const Art = () => (
+  <div className={styles.art}>Things that have been made.</div>
+);
---
+++ src/Components/Puzzle/PuzzlePiece/PuzzlePiece.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './PuzzlePiece.module.css';
+
+export const PuzzlePiece = ({
+  name,
+  link,
+  piece = <></>,
+  className = '',
+  children,
+  ...props
+}) => {
+  return React.Children.only(
+    React.cloneElement(children, {
+    })
+  );
+};
