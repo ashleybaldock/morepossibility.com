b8a93697590ffdb4f8b7693a84225d009c152567
ashleybaldock <ashleybaldock@users.noreply.github.com>
2020-06-21 15:45:54 +0000

    deploy: 5bf0fa7a3244eaa44977d2d36560667b7ce58d31

---
+++ precache-manifest.a902816ce7a2b02980e0ad3c6f07dc36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f23c2930448c1f59e20a993f1e00a13d",
+    "url": "https://ashleybaldock.github.io/morepossibility.com/index.html"
+  },
+  {
+    "revision": "ca1d2bae9f660b6043d9",
+    "url": "https://ashleybaldock.github.io/morepossibility.com/static/css/main.210ff56c.chunk.css"
+  },
+  {
+    "revision": "4d03e1f405c5ef64f9bf",
+    "url": "https://ashleybaldock.github.io/morepossibility.com/static/js/2.5a0d3fb5.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "https://ashleybaldock.github.io/morepossibility.com/static/js/2.5a0d3fb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca1d2bae9f660b6043d9",
+    "url": "https://ashleybaldock.github.io/morepossibility.com/static/js/main.3162fc74.chunk.js"
+  },
+  {
+    "revision": "fe9c66966ed1b40e2ec5",
+    "url": "https://ashleybaldock.github.io/morepossibility.com/static/js/runtime-main.a6491fdb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.11940154da796c1ebbc73d3601e457ea.js"
+  "https://ashleybaldock.github.io/morepossibility.com/precache-manifest.a902816ce7a2b02980e0ad3c6f07dc36.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://ashleybaldock.github.io/morepossibility.com/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
