d53bfbb623603aef678bea7be1a4b4752fce5416
Ashley Baldock <ashley@baldock.me>
2020-06-21 16:22:46 +0100

    Build and deploy

--- src/Components/TopNav/TopNav.js
+++ src/Components/TopNav/TopNav.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { FlexColumn, FlexRow, FlexSpacer } from "Components"; // TODO move
+import { Link } from "react-router-dom";
 import styles from "./TopNav.module.css";
 
 export const TopNav = ({ className = "", children, ...props }) => {
@@ -8,7 +9,7 @@ export const TopNav = ({ className = "", children, ...props }) => {
       <FlexRow flex={"0 0 auto"} className={styles.aboveSticky}></FlexRow>
       <FlexRow flex={"0 0 auto"} className={`${styles.topNav} ${className}`}>
         <FlexColumn justifyContent={"center"} className={styles.title}>
-          More Possibility
+          <Link to="/sitemap">More Possibility</Link>
         </FlexColumn>
         <FlexSpacer />
         {React.Children.map(children, (child) => (
