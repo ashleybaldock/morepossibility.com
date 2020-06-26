85feba530b67d36e1203985a5ec97bd9c72ddeec
Ashley Baldock <ashley@baldock.me>
2020-06-26 23:17:20 +0100

    Change copy

--- src/Pages/Welcome/Welcome.js
+++ src/Pages/Welcome/Welcome.js
@@ -15,9 +15,8 @@ export const Welcome = ({ className = '', ...props }) => {
         <PageParagraph>
           More Possibility is all about creating{' '}
           <Link to={`/tools`}>tools</Link> to enable artists to add light and
-          movement into their projects through the use of cheap, available LED
-          modules. Simple, intuitive, elegant software that enables powerful
-          expression.
+          movement to their projects through the use of inexpensive, widely available LED
+          modules coupled with an intuitive, powerful, and elegant software interface.
         </PageParagraph>
         <PageParagraph>
           I also use this system to create my own art – small scale, especially
@@ -25,7 +24,7 @@ export const Welcome = ({ className = '', ...props }) => {
           scale installations including the Phenoforge.
         </PageParagraph>
         <PageParagraph>
-          I also offer Software Development and Agile contract services.
+          I also offer Software Development and Agile contract services, specialising in React/node.js web and app development.
         </PageParagraph>
       </PageSection>
     </>
