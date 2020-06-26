4f88482eafc902ded0e58423dab6b1a65cab7742
ashleybaldock <ashleybaldock@users.noreply.github.com>
2020-06-26 22:12:39 +0000

    deploy: ecf0b9848a7620c567c88e317b53d69af00fc03d

---
+++ precache-manifest.028b2fb199e1d308f50fcf89e57c0716.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e25507a8b8a460c708d32bd01afa14a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1954814e8493a8812d5c",
+    "url": "/static/css/main.60c2cfb5.chunk.css"
+  },
+  {
+    "revision": "16a625a501f8eb903939",
+    "url": "/static/js/2.84ca798e.chunk.js"
+  },
+  {
+    "revision": "54fd6816419f7824e2804745930c9117",
+    "url": "/static/js/2.84ca798e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1954814e8493a8812d5c",
+    "url": "/static/js/main.904c0ec2.chunk.js"
+  },
+  {
+    "revision": "44c84cb0b656f984b7c4",
+    "url": "/static/js/runtime-main.73361b4b.js"
+  }
+]);
